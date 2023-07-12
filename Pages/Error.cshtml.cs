4e2fa8450df3dda38b43d45b1053296c4b1a3096
Alan Franco <alanfrancob97@gmail.com>
2023-07-12 15:51:02 -0600

    Added DbContext adn Student model scaffolding.
    - Scaffolding has been created for the Student model.
    - Added database context named SchoolContext.cs for the current
      entities: Student, Enrollment and Course.
    - Added database initializer named DbInitializer.cs to fill empty
      database with test data.

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -8,7 +8,7 @@ namespace ef_core_razor_tut_contoso_university.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
