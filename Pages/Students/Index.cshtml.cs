5f9ff2d54f6d53a776ce01520626e837880effc0
Alan Franco <alanfrancob97@gmail.com>
2023-07-28 10:21:31 -0600

    Added search bar name filter
    Added search bar to filter first, middle and last names.

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -26,13 +26,22 @@ namespace ContosoUniversity.Pages.Students
 
         public IList<Student> Students { get;set; }
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
             // using System;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
+            CurrentFilter = searchString;
+
             IQueryable<Student> studentsIQ = from s in _context.Students select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                // Remove ToUpper when working with SQL Server
+                studentsIQ = studentsIQ.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                    || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
 
             studentsIQ = sortOrder switch 
             {
